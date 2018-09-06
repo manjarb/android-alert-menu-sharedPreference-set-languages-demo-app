d91be0ba8f9528f7393c055f1b49025b9fb4b5e2
Varis Darasirikul <varis.dara@gmail.com>
2018-09-07 01:30:42 +0800

    Init

---
+++ app/src/main/java/com/amusexd/languagepreferences/MainActivity.java
@@ -0,0 +1,78 @@
+package com.amusexd.languagepreferences;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    TextView textView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.english) {
+            setLanguage("English");
+        } else
+        if (item.getItemId() == R.id.spanish) {
+            setLanguage("Spanish");
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        sharedPreferences = this.getSharedPreferences("com.amusexd.languagepreferences", Context.MODE_PRIVATE);
+        String language = sharedPreferences.getString("language", "");
+
+        if (language == "") {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_btn_speak_now)
+                    .setTitle("Choose a language")
+                    .setMessage("Which Language would you like?")
+                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setLanguage("English");
+                        }
+                    })
+                    .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setLanguage("Spanish");
+                        }
+                    })
+                    .show();
+        } else {
+            textView.setText(language);
+        }
+    }
+
+    public void setLanguage(String language) {
+        sharedPreferences = this.getSharedPreferences("com.amusexd.languagepreferences", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("language", language).apply();
+
+        textView.setText(language);
+    }
+}
